29b67cbc668bd1c310ccca678c249de09d6a207b
matei <mateibratu14@gmail.com>
2023-02-09 22:13:10 +0200

    Testing discord webhook

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -3,6 +3,6 @@ import * as rpc from 'rage-rpc'
 
 mp.events.add('playerReady', (player: PlayerMp) => {
     player.model = mp.joaat('player_zero');
-    player.spawn(new mp.Vector3(-425.517, 1123.620, 325.8544));
+    player.spawn(new mp.Vector3(-425.517, 1123.620, 325.8543));
     rpc.callClient(player, 'mti:authSystem')
 })
