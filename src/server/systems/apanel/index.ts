a3092d5e1978b16d1038aac16e0e6065da3d7a29
matei <mateibratu14@gmail.com>
2023-02-14 22:31:34 +0200

    AdminPanel: Added repairvehicle function Added repairvehicle function
    & small fix for the vehicle spawn

--- src/server/systems/apanel/index.ts
+++ src/server/systems/apanel/index.ts
@@ -1,5 +1,12 @@
 import * as rpc from 'rage-rpc'
 
+rpc.register('repairVehicle', () => {
+    const player = mp.players.at(0);
+    if (!player.vehicle) return player.sendToast({type: 'error', message: 'You need to be in a car to repair it'})
+    player.vehicle.repair();
+    player.sendToast({type: 'success', message: 'Vehicle repaired successfully'})
+})
+
 rpc.register('vehicleSpawn', () => {
     const player = mp.players.at(0);
     const car = mp.vehicles.new(0xEEA75E63, player.position,
@@ -8,6 +15,10 @@ rpc.register('vehicleSpawn', () => {
             numberPlate: 'ASDF',
             dimension: player.dimension
         });
+    if (player.vehicle) {
+        player.vehicle.destroy()
+    }
     player.putIntoVehicle(car, 0)
     player.sendToast({type: 'success', message: 'Vehicle spawned successfully'})
+
 })
